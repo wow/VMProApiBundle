fb44c1f508e4f5625734b70ec5918ca142be7d0e
Ruben Knol <ruben.knol@movingimage.com>
2016-11-26 09:32:54 +0100

    Another typo

--- Service/AbstractService.php
+++ Service/AbstractService.php
@@ -4,13 +4,13 @@ namespace MovingImage\Bundle\VMProApiBundle\Service;
 
 use MovingImage\Bundle\VMProApiBundle\Interfaces\ApiClientAwareInterface;
 use MovingImage\Bundle\VMProApiBundle\Interfaces\VideoManagerAwareInterface;
-use MovingImage\Bundle\VMProApiBundle\Traits\ApiClientWareTrait;
+use MovingImage\Bundle\VMProApiBundle\Traits\ApiClientAwareTrait;
 use MovingImage\Bundle\VMProApiBundle\Traits\VideoManagerAwareTrait;
 use MovingImage\Client\VMPro\Interfaces\ApiClientInterface;
 
 abstract class AbstractService implements ApiClientAwareInterface , VideoManagerAwareInterface
 {
-    use ApiClientWareTrait;
+    use ApiClientAwareTrait;
     use VideoManagerAwareTrait;
 
     public function __construct(ApiClientInterface $apiClient, $defaultVideoManagerId = null)
